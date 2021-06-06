3ec156a05601ba3ffc378278c4bd40347825a36c
Vicky-Jha <53377467+Vicky-Jha@users.noreply.github.com>
2021-06-07 00:04:09 +0530

    Update eulerTotientFun.cpp

--- eulerTotientFun.cpp
+++ eulerTotientFun.cpp
@@ -1,7 +1,42 @@
 #include<iostream>
 using namespace std;
+
 //Complexity O(sqrt(n))
-//counts total number of positive numbers which are coprime to n upto n from 2
+
+// Solution 1
+unordered_map<int, int> primeFactors(int n) {
+    unordered_map<int, int> factorization;
+
+    for(int i = 2; i <= n; ++i) {
+        if(n % i == 0) {
+            factorization[i] = 0;
+            while (n % i == 0) {
+                n /= i;
+                factorization[i]++;
+            }
+        }
+    }
+    return factorization;
+}
+
+
+int eulerTotientFunction(int n) {
+    int ETF = 1;
+    unordered_map<int, int> factorization = primeFactors(n);
+
+    for(auto factors : factorization) {
+        ETF *= powerFunc(factors.first, factors.second - 1) * (factors.first - 1); // formula
+    }
+
+    return ETF;
+}
+
+
+
+
+// Solution 2
+
+//counts total number of positive numbers which are coprime to n from 1 to n
 int phi(int n) {
     int result = n;
     for (int i = 2; i * i <= n; i++) {
@@ -16,16 +51,8 @@ int phi(int n) {
         result -= result/n;
     return result;
 }
-int main()
-{
-    int n;
-    cin>>n;
-    cout<<phi(n)<<"\n";
-    return 0;
-}
-
 
 //Explanation
 
 //https://cp-algorithms.com/algebra/phi-function.html
-//a^ϕ(m)≡1 (mod m) //Property of Euler totient function
+//a^ϕ(m)≡1 (mod m) //Property of Euler totient function // ϕ(m) = m - 1 if m is prime
