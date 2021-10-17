11862ecb30c824043c1e350ea87787ba8a8324a5
Nishi <nishi.nitb@gmail.com>
2021-10-17 22:49:52 +0530

    Added trailing_zeroes.cpp

---
+++ trailing_zeroes.cpp
@@ -0,0 +1,18 @@
+// since 5*2 = 10 and the number of 10s determines the number of trailing zeroes in the factorial
+// hence count the number of '5' in prime factorization of number
+// Thus, number of trailing zeroes = n/5 + n/25 + n/125 + n/625 + ....... till n >= 1
+
+int trailingZeroes(int n)
+{
+    // Factorial doesn't exist
+    if (n < 0)
+        return -1;
+
+    // Initialise the count variable
+    int count = 0;
+
+    for (int i = 5; n / i >= 1; i *= 5)
+        count += n / i;
+
+    return count;
+}
