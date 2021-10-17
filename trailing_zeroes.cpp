96bc47a504462f35a41338f768d518ef3a63ab50
Nishi <nishi.nitb@gmail.com>
2021-10-17 22:58:00 +0530

    update

--- trailing_zeroes.cpp
+++ trailing_zeroes.cpp
@@ -12,7 +12,7 @@ int trailingZeroes(int n)
     int count = 0;
 
     for (int i = 5; n / i >= 1; i *= 5)
-        count += n / i;  // increment the count with each power of 5 (n/i)
+        count += n / i;
 
     return count;
 }
